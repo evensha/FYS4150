5723a30e093ac38c53caeb6314f0ff7714b59daa
evensha <even.haland@gmail.com>
2017-09-22 17:31:26 +0200

    Restructured programs and made unit tests

---
+++ Project2/Programs/Jacobi_algorithm.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib> 
+#include <fstream>
+#include <iomanip>
+#include <armadillo>
+#include "time.h"
+
+using namespace std;
+using namespace arma; 
+
+void do_Jacobi(mat& A, mat& R, vec& lambda, int n); 
+double offdiag( mat A, int *p, int *q, int n ); // function that check value of the off-diagonal elements 
+void Jacobi_rotation( mat& A, mat& R, int k, int l, int n ); // function that does the rotation 
+
+void Jacobi_tests(); 
+
