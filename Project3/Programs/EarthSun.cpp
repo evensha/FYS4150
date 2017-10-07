3383a6a32ec0ff04fb215bb82f6103b797d668ff
evensha <even.haland@gmail.com>
2017-10-07 14:04:57 +0200

    Started Project 3

---
+++ Project3/Programs/EarthSun.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib> 
+#include <fstream>
+#include <string>
+#include <iomanip>
+//#include <armadillo>
+#include "time.h"
+
+
+using namespace std;
+//using namespace arma; 
+
+ofstream ofile;  
+
+int main(int argc, char *argv[]){ 
+
+	int n = atoi(argv[1]); 
+	double t_0 = 0.0; 
+	double t_n = 1.0; 
+	double h = (t_0 - t_n)/n; 
+	
+	
+
+	// Forward Euler 
+
+
+
+	// Velocity Verlet  
+	
+
+
+
+
+
+
+
+
+} 
