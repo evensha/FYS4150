feee2dceb0352bfd5ca77763edf67b8d7628e0e7
evensha <even.haland@gmail.com>
2017-11-16 15:06:50 +0100

    4d and parallelizing

--- Project4/Programs/Plotting.py
+++ Project4/Programs/Plotting.py
@@ -3,46 +3,174 @@ from numpy import *
 import sys
 
 spins = sys.argv[1]
-temp = sys.argv[2] 
+temps = [1, 2.4]
+#temp = sys.argv[2] 
 
 
-infile = open("Output/Output_"+spins+"_"+temp+".txt")
+infile1 = open("Output/Output_"+spins+"_1.txt")
+infile2 = open("Output/Output_"+spins+"_2.4.txt")
 
-infile.readline()
+infile1.readline()
+infile2.readline()
 
-t = []
-E = []
-M = []
+t1 = []
+E1 = []
+E1mean = []
+M1 = []
+M1mean = []
+a1 = []
 
-for line in infile: 
+t2 = []
+E2 = []
+E2mean = []
+M2 = []
+M2mean = []
+a2 = []
+
+for line in infile1: 
 	words = line.split()
-	t.append(float(words[0]))
-	E.append(float(words[1]))
-	M.append(float(words[2]))
+	t1.append(float(words[0]))
+	E1.append(float(words[1]))
+	E1mean.append(float(words[2]))
+	M1.append(float(words[3]))
+	M1mean.append(float(words[4]))
+	a1.append(float(words[5]))
 
-infile.close()
+for line in infile2: 
+	words = line.split()
+	t2.append(float(words[0]))
+	E2.append(float(words[1]))
+	E2mean.append(float(words[2]))
+	M2.append(float(words[3]))
+	M2mean.append(float(words[4]))
+	a2.append(float(words[5]))
 
-t = array(t) 
-E = array(E) 
-M = array(M) 
+infile1.close()
+infile2.close()
 
+t1 = array(t1) 
+E1 = array(E1) 
+E1mean = array(E1mean)
+M1 = array(M1) 
+M1mean = array(M1mean)
+a1 = array(a1)
+
+t2 = array(t2) 
+E2 = array(E2) 
+E2mean = array(E2mean)
+M2 = array(M2) 
+M2mean = array(M2mean)
+a2 = array(a2)
+
+"""
 figure() 
-plot(t, E) 
+plot(t1, E1mean) 
 xlabel('# MC cycles')
 ylabel('Energy')
-title('L=20, T=%s' %temp)
-#xlim(0,10000)
-#ylim(-800, -796)
-show() 
-savefig("Output/Energy_"+spins+"_"+temp+".png")
+title('L=20, T=1')
+#xlim(0,50000)
+#ylim(-525, -475)
+#show() 
+savefig("Output/Energy_"+spins+"_1.png")
 
 figure() 
-plot(t, M) 
+plot(t1, M1mean) 
 xlabel('# MC cycles')
 ylabel('Magnetization')
-title('L=20, T=%s' %temp)
+title('L=20, T=1')
 #xlim(0,10000)
 #ylim(399,400)
 #show() 
-savefig("Output/Magnetization_"+spins+"_"+temp+".png")
+savefig("Output/Magnetization_"+spins+"_1.png")
+
+figure() 
+plot(t2, E2mean) 
+xlabel('# MC cycles')
+ylabel('Energy')
+title('L=20, T=2.4')
+#xlim(0,50000)
+#ylim(-525, -475)
+#show() 
+savefig("Output/Energy_"+spins+"_2.4.png")
+
+figure() 
+plot(t2, M2mean) 
+xlabel('# MC cycles')
+ylabel('Magnetization')
+title('L=20, T=2.4')
+#xlim(0,10000)
+#ylim(399,400)
+#show() 
+savefig("Output/Magnetization_"+spins+"_2.4.png")
+
+figure()
+plot(t1,a1) 
+plot(t1,a2)
+legend(['T = 1', 'T=2.4'])
+xlabel('# MC cycles')
+ylabel('Accepted configurations')
+#show()
+savefig("Output/Accepted_configurations_"+spins+".png")
+
+data = E1[30000:-1]
+binwidth = 0.001
+
+figure()
+hist(data,bins = arange(min(data), max(data)+binwidth, binwidth))
+savefig("Output/Hist_energies.png")
+"""
+
+Energies1 = unique(E1[200000:-1])
+Energies2 = unique(E2[200000:-1])
+E1 = E1[200000:-1]
+E2 = E2[200000:-1]
+t = t1[200000:-1]
+
+prob_dist1 = {}
+prob_dist2 = {}
+
+for i in range(len(Energies1)): 
+	prob_dist1[str(Energies1[i])] = 0 
+
+for i in range(len(Energies2)): 
+	prob_dist2[str(Energies2[i])] = 0 
+
+norm = len(t)
+for i in range(len(t)): 
+	prob_dist1[str(E1[i])] += 1.0/norm
+	prob_dist2[str(E2[i])] += 1.0/norm
+
+p1 = []
+#print 'PDF for T = 1.0:'
+for E in Energies1:
+	p = prob_dist1[str(E)]
+	p1.append(p) 
+	#print 'P(%d) = %f' %(E,p)
+
+p2 = []
+#print 'PDF for T = 2.4'
+for E in Energies2:
+	p = prob_dist2[str(E)] 
+	p2.append(p)
+	#print 'P(%d) = %f' %(E,p)
+
+p1 = array(p1) 
+p2 = array(p2)
+
+figure()
+plot(Energies1,p1)
+xlabel('E')
+ylabel('P(E)')
+savefig('Output/Energy_PDF_T=1.png')
+
+figure()
+plot(Energies2,p2)
+xlabel('E')
+ylabel('P(E)')
+savefig('Output/Energy_PDF_T=2.4.png')
+
+
+
+
+
 
--- Project4/Programs/main.cpp
+++ Project4/Programs/main.cpp
@@ -7,12 +7,12 @@
 #include <armadillo>
 #include "time.h"
 #include "lib.h"
-//#include "mpi.h"
+#include "mpi.h"
 
 using namespace std;
 using namespace arma; 
 
-ofstream ofile; 
+ofstream ofile, ofile1; 
 
 inline int periodic(int i, int limit, int add){
 	return (i+limit+add) % (limit); 
@@ -20,7 +20,7 @@ inline int periodic(int i, int limit, int add){
 
 
 void initialize(int, double, mat, double&, double&);
-void Metropolis(int, long&, mat&, double&, double&, vec); 
+void Metropolis(int, long&, mat&, double&, double&, int&, vec); 
 void output(int, int, double, vec); 
 
 int main(int argc, char *argv[]){ 
@@ -39,8 +39,8 @@ int main(int argc, char *argv[]){
 	vec w = zeros<vec>(33); 
 	vec average = zeros<vec>(5); 
 	vec total_average = zeros<vec>(5); 
-	int my_rank = 0; 
-	/*
+	//int my_rank = 0; 
+	
 	// MPI initialization 
 	int my_rank, numprocs; 
 	MPI_Init(&argc, &argv); 
@@ -49,8 +49,19 @@ int main(int argc, char *argv[]){
 	if(my_rank == 0 && argc <= 1){
 		cout << "Bad usage: " << argv[0] << " read output file" << endl; exit(1); 
 	}
-	
+	if( my_rank == 0 && argc > 1 ){
+		ostringstream os1; 
+		os1 << "Output/Output_L_" << n_spins << ".txt"; 
+		string outfile1 = os1.str(); 
+		ofile1.open(outfile1.c_str()); 
+	}
 
+	if( n_spins == 20){
+		 initial_temp = 1.0; final_temp = 2.4; temp_step = 1.4; 
+	}
+	else{ 
+		initial_temp = 2.0; final_temp = 2.3; temp_step = 0.05; 
+	}
 
 	int no_intervalls = mcs/numprocs; 
 	int myloop_begin = my_rank*no_intervalls +1; 
@@ -67,11 +78,11 @@ int main(int argc, char *argv[]){
 	idum = -1-my_rank; 
 	double TimeStart, TimeEnd, TotalTime; 
 	TimeStart = MPI_Wtime(); 
-	*/
-	initial_temp = 1.0; final_temp = 2.4; temp_step = 1.4; 
+
+	int acc_confs; 
 	for(double temp = initial_temp; temp <= final_temp; temp+=temp_step){
 
-		E = M = 0; 	
+		E = M = acc_confs = 0; 	
 		spin_matrix = ones<mat>(n_spins, n_spins);
 		if( rand_spins  == 1 ){
 			spins = "random"; 
@@ -79,7 +90,8 @@ int main(int argc, char *argv[]){
 				for(int y = 0; y<n_spins; y++){
 					double s = ran1(&idum); 
 					if( s < 0.5 ) spin_matrix(x,y) = -1; 
-					if( s > 0.5 ) spin_matrix(x,y) = 1; 
+					else spin_matrix(x,y) = 1; 
+					//cout << spin_matrix(x,y) << endl; 
 				}
 			}
 		}
@@ -87,6 +99,7 @@ int main(int argc, char *argv[]){
 		for( int de = -16; de <= 16; de++) w(de+16) = 0; 
 		for( int de = -16; de <= 16; de+=8) w(de+16) = exp(-de/temp); 
 		for( int i = 0; i<5; i++) average(i) = 0; 
+		for( int i = 0; i<5; i++) total_average(i) = 0; 
 
 		initialize(n_spins, temp, spin_matrix, E, M);	
 		/*
@@ -95,19 +108,24 @@ int main(int argc, char *argv[]){
 		cout << "Energy: " << E << endl; 
 		cout << "Magnetization: " << M << endl; 
 		*/
-		ostringstream os; 
-		os << "Output/Output_" << spins << "_" << temp << ".txt"; 
-		string outfile = os.str(); 
-		ofile.open(outfile.c_str()); 
-		ofile << "Time" << setw(20) << "Energy" << setw(20) << "Magnetization" << endl;  
-
-		for( int cycles = 1; cycles <= mcs; cycles++){
-			Metropolis(n_spins, idum, spin_matrix, E, M, w); 
-			average(0) += E; average(1) += E*E; average(2) += M; average(3) += M*M; average(4) += fabs(M); 
-			//output(n_spins, mcs, temp, total_average); 
-			ofile << cycles << setw(20) << average(0)/((double) cycles) << setw(20) << average(4)/((double) cycles) << endl; 
+		if( n_spins == 20){
+			ostringstream os; 
+			os << "Output/Output_" << spins << "_" << temp << ".txt"; 
+			string outfile = os.str(); 
+			ofile.open(outfile.c_str()); 
+			ofile << "MCs" << setw(20) << "E" << setw(20) << "meanE" << setw(20) << "M" << setw(20) << "meanM" << setw(20) << 
+			"Accepted configs" << endl;  
 		}
-		/*	
+	
+		for( int cycles = myloop_begin; cycles <= myloop_end; cycles++){
+			Metropolis(n_spins, idum, spin_matrix, E, M, acc_confs, w); 
+			average(0) += E; average(1) += E*E; average(2) += M; average(3) += M*M; average(4) += fabs(M); 
+			if( n_spins == 20){
+			ofile << cycles << setw(20) << E << setw(20) << average(0)/((double) cycles) << setw(20) << 
+																		 M << setw(20) << average(4)/((double) cycles) << setw(20) << acc_confs << endl; 
+			}
+		}
+
 		// Find total average
 		for( int i = 0; i<5; i++){
 			MPI_Reduce(&average(i), &total_average(i), 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); 
@@ -116,15 +134,16 @@ int main(int argc, char *argv[]){
 		if( my_rank == 0 ){
 			output(n_spins, mcs, temp, total_average); 
 		}
-		*/
-		ofile.close(); 
-		//TimeEnd = MPI_Wtime(); 
-		//TotalTime = TimeEnd - TimeStart; 
+		
+		//output(n_spins, mcs, temp, average); 
+		if(n_spins == 20) ofile.close(); 
+		TimeEnd = MPI_Wtime(); 
+		TotalTime = TimeEnd - TimeStart; 
 		
 		if( my_rank == 0 ){
 
 			cout << "--------------------" << endl; 
-			//cout << "Time = " << TotalTime << " on number of processors: " << numprocs << endl; 
+			cout << "Time = " << TotalTime << " on number of processors: " << numprocs << endl; 
 			cout << "Temperature = " << temp << endl; 	
 
 			for( int i = 0; i<5; i++) average(i) = average(i)/((double) mcs); 	
@@ -141,9 +160,11 @@ int main(int argc, char *argv[]){
 
 		}
 		
+
 	}	
 
-	//MPI_Finalize(); 
+	MPI_Finalize(); 
+	ofile1.close(); 
 	return 0; 
 
 }
@@ -154,14 +175,14 @@ void initialize(int n_spins, double temperature, mat spin_matrix, double &E, dou
 	for(int i = 0; i<n_spins; i++){
 		for(int j = 0; j<n_spins; j++){
 			M += (double) spin_matrix(i,j); 
-			E -= (double) spin_matrix(i,j)*( spin_matrix(periodic(j,n_spins,-1), i) + spin_matrix(j, periodic(i,n_spins,-1) )  ); 
+			E -= (double) spin_matrix(i,j)*( spin_matrix(periodic(i,n_spins,-1), j) + spin_matrix(i, periodic(j,n_spins,-1) )  ); 
 		}
 	}
 
 }
 
 
-void Metropolis(int n_spins, long& idum, mat& spin_matrix, double& E, double& M, vec w){
+void Metropolis(int n_spins, long& idum, mat& spin_matrix, double& E, double& M, int& a, vec w){
 
 	for(int y = 0; y<n_spins; y++){
 		for(int x = 0; x<n_spins; x++){
@@ -169,7 +190,9 @@ void Metropolis(int n_spins, long& idum, mat& spin_matrix, double& E, double& M,
 			int iy = (int) (ran1(&idum)*(double)n_spins); 
 			int deltaE = 2*spin_matrix(iy,ix)*(  spin_matrix(iy, periodic(ix,n_spins,-1)) + spin_matrix(periodic(iy,n_spins,-1),ix)	
 										 										+	 spin_matrix(iy, periodic(ix,n_spins, 1)) + spin_matrix(periodic(iy,n_spins, 1),ix) );
+			//if( deltaE == 32 ) cout << "What!!!" << endl; 
 			if( ran1(&idum) <= w(deltaE + 16) ){
+				a++; 
 				spin_matrix(iy,ix) *= -1; 
 				M += (double) 2*spin_matrix(iy, ix); 
 				E += (double) deltaE; 
@@ -182,7 +205,7 @@ void Metropolis(int n_spins, long& idum, mat& spin_matrix, double& E, double& M,
 
 
 void output(int n_spins, int mcs, double temperature, vec total_average){
-
+	//cout << "Inside output function!!" << endl; 
 	double norm = 1/((double) (mcs)); 
 	double Etotal_average = total_average(0)*norm; 
 	double E2total_average = total_average(1)*norm; 
@@ -190,16 +213,16 @@ void output(int n_spins, int mcs, double temperature, vec total_average){
 	double M2total_average = total_average(3)*norm; 
 	double Mabstotal_average = total_average(4)*norm; 
 
-	double Evariance = (E2total_average - Etotal_average*Etotal_average)/n_spins/n_spins; 
-	double Mvariance = (M2total_average - Mtotal_average*Mtotal_average)/n_spins/n_spins; 
+	double Evariance = (E2total_average - Etotal_average*Etotal_average); ///n_spins/n_spins; 
+	double Mvariance = (M2total_average - Mtotal_average*Mtotal_average); ///n_spins/n_spins; 
 
-	ofile << setiosflags(ios::showpoint | ios::uppercase); 
-	ofile << setw(15) << setprecision(8) << temperature; 
-	ofile << setw(15) << setprecision(8) << Etotal_average/n_spins/n_spins;
-	ofile << setw(15) << setprecision(8) << Evariance/temperature/temperature; 
-	ofile << setw(15) << setprecision(8) << Mtotal_average/n_spins/n_spins; 
-	ofile << setw(15) << setprecision(8) << Mvariance/temperature; 
-	ofile << setw(15) << setprecision(8) << Mabstotal_average/n_spins/n_spins << endl; 
+	ofile1 << setiosflags(ios::showpoint | ios::uppercase); 
+	ofile1 << setw(15) << setprecision(8) << temperature; 
+	ofile1 << setw(15) << setprecision(8) << Etotal_average; ///n_spins/n_spins;
+	ofile1 << setw(15) << setprecision(8) << Evariance/temperature/temperature; 
+	ofile1 << setw(15) << setprecision(8) << Mtotal_average; ///n_spins/n_spins; 
+	ofile1 << setw(15) << setprecision(8) << Mvariance/temperature; 
+	ofile1 << setw(15) << setprecision(8) << Mabstotal_average << endl; ///n_spins/n_spins << endl; 
 
 }
 
